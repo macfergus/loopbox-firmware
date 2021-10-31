b9f6ca094ae6880a307998540f342eb8ac7581c6
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2021-10-31 14:02:41 -0700

    Create minimum build system

---
+++ Makefile
@@ -0,0 +1,58 @@
+CXX=arm-none-eabi-g++
+CC=arm-none-eabi-gcc
+LD=arm-none-eabi-g++
+OBJCOPY=arm-none-eabi-objcopy
+
+CFLAGS=-g \
+    -D__SAMD21G15A__ \
+    -Iinclude \
+    -fdata-sections -ffunction-sections \
+    -mabi=aapcs -flto -mthumb -mcpu=cortex-m0plus
+CXXFLAGS=-std=c++20 $(CFLAGS)
+LDFLAGS=-flto -mthumb -mcpu=cortex-m0plus --specs=nosys.specs -Wl,--gc-sections
+
+SRCDIR=src
+BUILDDIR=build
+CPP_OBJDIR=$(BUILDDIR)/cpp
+C_OBJDIR=$(BUILDDIR)/c
+
+CPP_SRCS := $(shell find $(SRCDIR) -name '*.cpp')
+CPP_OBJS := $(addprefix $(CPP_OBJDIR)/,${CPP_SRCS:.cpp=.o})
+
+C_SRCS=
+C_OBJS=$(addprefix $(C_OBJDIR)/,${C_SRCS:.c=.o})
+
+OBJS=$(CPP_OBJS) $(C_OBJS)
+
+BIN=$(BUILDDIR)/loopbox.bin
+ELF=${BIN:.bin=.elf}
+MAP=$(ELF).map
+LDSCRIPT=samd21.ld
+
+.PHONY: all
+all: $(BIN)
+
+.PHONY: clean
+clean:
+	rm -f $(OBJS) $(ELF) $(BIN) $(MAP)
+	find $(BUILDDIR) -name '*.o' -delete
+
+$(CPP_OBJDIR)/%.o: %.cpp
+	@mkdir -p $(dir $@)
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+$(C_OBJDIR)/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(ELF): $(OBJS) $(LDSCRIPT)
+	@mkdir -p $(dir $@)
+	$(LD) $(LDFLAGS) -T $(LDSCRIPT) -Wl,-Map=$(MAP) $(OBJS) -o $(ELF)
+
+%.bin: %.elf
+	@mkdir -p $(dir $@)
+	$(OBJCOPY) -O binary $< $@
+
+.PHONY: flash
+flash: $(BIN) flash.jlink
+	JLinkExe -device ATSAMD21G15 -if SWD -speed 4000 -autoconnect 1 -CommanderScript flash.jlink
---
+++ src/vectors.cpp
@@ -0,0 +1,136 @@
+#include <cstddef>
+#include <cstdint>
+
+// The "practical" entrypoint for your program
+extern int main();
+
+// The linker script must define these values
+extern uint32_t __stack_top__;
+// This is where global data comes from (in flash??)
+extern uint32_t __etext__;
+// This is where global data lands (in sram??)
+extern uint32_t __data_start__;
+extern uint32_t __data_end__;
+// This is where globals that should be zeroed out go
+extern uint32_t __bss_start__;
+extern uint32_t __bss_end__;
+
+// The start of memory must contain this interrupt vector table. The processor
+// will use it to figure out where to start execution
+// Ref: https://developer.arm.com/documentation/dui0552/a/the-cortex-m3-processor/exception-model/vector-table
+using InterruptHandler = void (*)();
+
+struct isr_vectors {
+    // Pointer to the stack
+    void* stack; // 0x0
+    // This is were code starts execution
+    InterruptHandler reset; // 0x04
+    // The rest are interrupt handlers
+    InterruptHandler nmi; // 0x08
+    InterruptHandler hard_fault; // 0x0c
+    InterruptHandler mem_mgmt_fault; // 0x10
+    InterruptHandler bus_fault; // 0x14
+    InterruptHandler usage_fault; // 0x18
+    InterruptHandler reserved1; // 0x1c
+    InterruptHandler reserved2; // 0x20
+    InterruptHandler reserved3; // 0x24
+    InterruptHandler reserved4; // 0x28
+    InterruptHandler sv_call; // 0x2c
+    InterruptHandler reserved_debug; // 0x30
+    InterruptHandler reserved_debug2; // 0x34
+    InterruptHandler pend_sv; // 0x38
+    InterruptHandler systick; // 0x3c
+
+    InterruptHandler pm; // irq 0
+    InterruptHandler sysctrl; // irq 1
+    InterruptHandler wdt; // irq 2
+    InterruptHandler rtc; // irq 3
+    InterruptHandler eic; // irc 4
+    InterruptHandler nvmctrl; // irq 5
+    InterruptHandler dmac; // irq 6
+    InterruptHandler usb; // irq 7
+    InterruptHandler evsys; // irq 8
+};
+
+// Do-nothing interrupt handler
+void dummyHandler() {
+    return;
+}
+
+// Interrupt handler that freezes the CPU
+void freeze() {
+    while (1) {}
+}
+
+// This is where execution starts. For our simple program, we just jump right
+// into main()
+// In a more complicated program, you might have to do some system setup, or
+// initialize globals (e.g., copying initial values from rom, calling
+// constructors, etc)
+extern "C" void __libc_init_array(void);
+extern "C" {
+void resetHandler() {
+    uint32_t *dest;
+    uint32_t *src;
+
+    // Copy the data section
+    dest = &__data_start__;
+    src = &__etext__;
+    // why is this condition necessary???
+    if (src != dest) {
+        while (dest != &__data_end__) {
+            *dest = *src;
+            ++dest;
+            ++src;
+        }
+    }
+
+    // Zero out .bss section
+    dest = &__bss_start__;
+    while (dest != &__bss_end__) {
+        *dest = 0;
+        ++dest;
+    }
+
+    // Call ctors
+    __libc_init_array();
+
+    main();
+
+    // If main ever exits, just spin afterwards
+    while (true) {}
+}
+}
+
+// This table needs to live in a specific place in memory.
+// __attribute__ ((section)) marks this struct for special handling in the
+// linker.
+// Then your linker script, you can specify the memory location for the
+// .isr_vector section.
+__attribute__ ((section(".isr_vectors"), used))
+const struct isr_vectors interrupt_vector_table = {
+    .stack = &__stack_top__,
+    .reset = resetHandler,
+    .nmi = freeze,
+    .hard_fault = freeze,
+    .mem_mgmt_fault = freeze,
+    .bus_fault = freeze,
+    .usage_fault = freeze,
+    .reserved1 = nullptr,
+    .reserved2 = nullptr,
+    .reserved3 = nullptr,
+    .reserved4 = nullptr,
+    .sv_call = freeze,
+    .reserved_debug = nullptr,
+    .pend_sv = nullptr,
+    .systick = dummyHandler,
+    .pm = freeze,
+    .sysctrl = freeze,
+    .wdt = dummyHandler,
+    .rtc = dummyHandler,
+    .eic = dummyHandler,
+    .nvmctrl = dummyHandler,
+    .dmac = dummyHandler,
+    .usb = dummyHandler,
+    .evsys = dummyHandler,
+};
